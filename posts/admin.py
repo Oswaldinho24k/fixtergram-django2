6ed0e38d6de8d0464a640d0a9591179c30771509
Oswaldinho <oswalfut_96@hotmail.com>
2018-04-05 18:14:38 -0500

    can add user posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Tag
 
 class PostAdmin(admin.ModelAdmin):
 	list_display= ['title', 'author', 'created']
@@ -8,3 +8,4 @@ class PostAdmin(admin.ModelAdmin):
 	
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
 class Post(models.Model):
+
 	author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
 	title = models.CharField(max_length=280)
+	tags = models.ManyToManyField(Tag, related_name='posts')
 	image = models.ImageField(upload_to='posts')
 	text = models.TextField()
 	created = models.DateTimeField(auto_now_add=True)
